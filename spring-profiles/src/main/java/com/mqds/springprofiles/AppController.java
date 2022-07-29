e524fbf47634adbde0ea2895baf84d10622e04a3
Marcelo Queiroz de Santana <marcelqdstrab@gmail.com>
2022-07-29 19:49:14 -0300

    criação de propriedades via properties.yaml variável de ambiente

--- spring-profiles/src/main/java/com/mqds/springprofiles/AppController.java
+++ spring-profiles/src/main/java/com/mqds/springprofiles/AppController.java
@@ -10,8 +10,15 @@ public class AppController {
     @Value("${app.message}")
     private String appMessage;
 
+    @Value("${ENV_DB_URL:NENHUMA}")
+    private String dbEnvironmentVariable;
+
     @GetMapping("/")
     public String getAppMessage(){
         return appMessage;
+    }
+    @GetMapping("/env")
+    public String getEnvironmentVariable(){
+        return "Essa é a variável via ambiente: "+ dbEnvironmentVariable;
     }
 }
