7d415eff142d725d3f11359c800a299375df93a0
Marcelo Queiroz de Santana <marcelqdstrab@gmail.com>
2022-07-29 17:25:00 -0300

    criação projeto base spring boot - helloWorld

---
+++ projeto-spring/src/test/java/com/mqds/projetospring/ProjetoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mqds.projetospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
